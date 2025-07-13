951644e791c83a845c48c66bded1c608056e90f6
Alan <87167180+Idraliski@users.noreply.github.com>
2025-07-13 15:19:25 +0800

    Added song length and music commands to beat manager

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -32,23 +32,26 @@ public struct MusicEvent
     public string name;
     public EventInstance reference;
     public BeatData data;
+    public int length;
 }
 
 public class BeatManager : MonoBehaviour
 {
-    [SerializeField] private List<EventReference> music;
+    [SerializeField] private List<EventReference> musicFile;
 
     private readonly List<MusicEvent> musicData = new();
 
+    [SerializeField] private float musicVolume = 1.0f;
+
     void Awake()
     {
-        if (music == null || music.Count == 0)
+        if (musicFile == null || musicFile.Count == 0)
         {
             Debug.LogError("No music events assigned in BeatManager.");
             DestroyImmediate(gameObject);
         }
 
-        foreach (var musicEvent in music)
+        foreach (var musicEvent in musicFile)
         {
             string eventName = musicEvent.Path[(musicEvent.Path.LastIndexOf('/') + 1)..];
             string path = Path.Combine(Application.streamingAssetsPath, eventName + ".json");
@@ -66,12 +69,22 @@ public class BeatManager : MonoBehaviour
                 continue;
             }
 
+            var instance = RuntimeManager.CreateInstance(musicEvent);
+            EventDescription description;
+            instance.getDescription(out description);
+            int size = 0;
+            description.getLength(out size);
+
             MusicEvent musicEventInstance = new MusicEvent
             {
                 name = eventName,
-                reference = RuntimeManager.CreateInstance(musicEvent),
-                data = beatData
+                reference = instance,
+                data = beatData,
+                length = size
             };
+            
+
+            musicEventInstance.reference.setVolume(musicVolume);
 
             musicData.Add(musicEventInstance);
         }
@@ -79,27 +92,27 @@ public class BeatManager : MonoBehaviour
 
     void Start()
     {
-        PlayMusic();
+
     }
 
-    public void PlayMusic()
+    void OnDestroy()
     {
-        foreach (var eventReference in music)
+        for (int i = 0; i < musicData.Count; i++)
         {
-            EventInstance instance = RuntimeManager.CreateInstance(eventReference);
-            instance.setVolume(0.5f);
-            instance.start();
+            DestroyMusic(i);
         }
     }
 
-    public void StopMusic()
+    public void StartMusic(int index) => musicData[index].reference.start();
+    public void PauseMusic(int index) => musicData[index].reference.setPaused(true);
+    public void UnPauseMusic(int index) => musicData[index].reference.setPaused(false);
+    public void FadeStopMusic(int index) => musicData[index].reference.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    public void ImmediateStopMusic(int index) => musicData[index].reference.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    public void ReleaseMusic(int index) => musicData[index].reference.release();
+    public void DestroyMusic(int index)
     {
-        foreach (var eventReference in music)
-        {
-            EventInstance instance = RuntimeManager.CreateInstance(eventReference);
-            instance.setVolume(0.5f);
-            instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
-        }
+        ImmediateStopMusic(index);
+        ReleaseMusic(index);
     }
 
     public List<MusicEvent> GetMusicData()
